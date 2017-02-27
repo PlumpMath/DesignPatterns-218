09eaac628449e2c5fa43bfee840663ac79b10f24
mehmet_cinci@hotmail.com <mehmet_cinci@hotmail.com>
2017-02-27 22:37:03 +0300

    Design Patterns Start
    Design Patterns Start

---
+++ Creational.Abstract.Factory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Abstract.Factory {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Creational.Abstract.Factory Start");
+            Console.WriteLine("Creational.Abstract.Factory End");
+            Console.Read();
+        }
+    }
+}
