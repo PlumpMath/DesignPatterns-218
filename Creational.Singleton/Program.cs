e1c1065a56526f0d011fbaea757b5426839f986b
mehmet_cinci@hotmail.com <mehmet_cinci@hotmail.com>
2017-02-28 23:21:14 +0300

    A class of which only a single instance can exist

--- Creational.Singleton/Program.cs
+++ Creational.Singleton/Program.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// A class of which only a single instance can exist 
+/// </summary>
 namespace Creational.Singleton {
     class Program {
         static void Main(string[] args) {
-            // MEHMET CİNCİ
-            // KEMAL ÖZTÜRK
+            System.Data.SqlClient.SqlConnection connectionFirst = SqlConnectionSingleton.Connection;
+            Console.WriteLine("Connection First  : {0}", connectionFirst.GetHashCode());
+            System.Data.SqlClient.SqlConnection connectionSecond = SqlConnectionSingleton.Connection;
+            Console.WriteLine("Connection Second : {0}", connectionSecond.GetHashCode());
+            Console.Read();
         }
     }
 }
---
+++ Creational.Singleton/SqlConnectionSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Creational.Singleton {
+    public class SqlConnectionSingleton {
+        private static volatile SqlConnection connection;
+        private static object syncRoot = new Object();
+        private static Guid HashCode;
+
+        private SqlConnectionSingleton() {
+            HashCode = Guid.NewGuid();
+        }
+
+        public static SqlConnection Connection {
+            get {
+                if (connection == null) {
+                    lock (syncRoot) {
+                        if (connection == null)
+                            connection = new SqlConnection();
+                    }
+                }
+                return connection;
+            }
+        }
+
+        public string GetString() {
+            return HashCode.ToString();
+        }
+    }
+}
